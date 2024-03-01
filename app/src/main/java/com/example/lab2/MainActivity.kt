94649eacbaf8fd03ebfc4d8e78fe95a98d7e4546
Kashif Ali <kashifali.bscssef20@iba-suk.edu.pk>
2024-03-02 00:56:08 +0500

    Task 2 Added

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab2.ui.theme.Lab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,29 +38,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("")
                 }
             }
         }
     }
 }
 
-// Task 1
+// Task 2
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier.fillMaxSize()
+    Row(
+        modifier = Modifier
+            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(10.dp))
+            .fillMaxWidth()
+            .padding(10.dp)
     ) {
-        var buttonColor = remember { mutableStateOf(Color.Blue) }
-
-        Button(
-
-            onClick = { buttonColor.value = Color.Red },
-            colors = ButtonDefaults.buttonColors(containerColor = buttonColor.value)
+        Image(
+            painter = painterResource(id = R.drawable.removedbg),
+            contentDescription = null,
+            modifier = Modifier
+                .weight(0.5f)
+                .align(alignment = Alignment.CenterVertically)
+        )
+        Column(
+            modifier = Modifier.weight(1f)
         ) {
-            Text(text = "Click Me")
+            var text by remember { mutableStateOf("") }
 
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newText -> text = newText },
+                label = { Text(text = "Enter Name") }
+            )
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newText -> text = newText },
+                label = { Text(text = "Phone Number") }
+            )
         }
     }
 }
@@ -59,6 +85,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Lab2Theme {
-        Greeting("Android")
+        Greeting("")
     }
 }
